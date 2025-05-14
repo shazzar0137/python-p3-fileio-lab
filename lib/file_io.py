f8c34e7f16f4b8f78dee8aec5972e3655f8db4aa
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-14 15:15:43 +0300

    updates fike_io.py with read,write/append methods

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", mode="w") as file:
+        file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", mode="a") as file:
+        file.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f"{file_name}.txt") as file:
+        return file.read()
     pass
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+print(read_file(file_name="logfile"))
